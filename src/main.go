043582a15a548dede434fe42c1030d55e1ef62c9
nicknoonan <47186822+nicknoonan@users.noreply.github.com>
2025-05-06 23:22:52 -0400

    setup executable

--- src/main.go
+++ src/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"os"
+	"log"
+)
+
+func main() {
+	args := os.Args
+	if len(args) > 1 {
+		contentBytes, err := os.ReadFile(args[1])
+		if err != nil {
+			log.Fatalf("Error while loading test file: %v", err)
+		}
+		result, err := Parse(string(contentBytes))
+		if err != nil {
+			log.Fatalf("Error while parsing: %v", err)
+		}
+		log.Println(result)
+	}
+}
