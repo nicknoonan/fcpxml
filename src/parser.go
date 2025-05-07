d5e0ad996787b8a4ad6bf4b3bfbc592a197afe50
nicknoonan <47186822+nicknoonan@users.noreply.github.com>
2025-05-06 23:23:15 -0400

    try out a xml package

--- src/parser.go
+++ src/parser.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"log"
+	"github.com/clbanning/mxj/v2"
+)
+
 func Parse(contents string) (string, error) {
-	return "", nil
+	mymap, err := mxj.NewMapXml([]byte(contents))
+	if err != nil {
+		return "", err
+	}
+	// log.Println(mymap.StringIndent())
+	//mymap.PathsForKey("chapter-marker")
+	log.Println(mymap.PathsForKey("chapter-marker"))
+	log.Println(mymap.ValuesForPath("fcpxml.library.event.project.sequence.spine.asset-clip.chapter-marker"))
+	return mymap.StringIndent(), nil
 }
